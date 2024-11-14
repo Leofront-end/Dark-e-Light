182436b20b97322930a7c161ba1d8bd970e4ae85
Leofront-end <ls171258@gmail.com>
2024-11-14 20:00:40 -0300

    falta apenas trocar img e o texto p

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 let trilho = document.getElementById("trilho")
 let body = document.querySelector('body')
+let titulo = document.querySelector('h1')
 
 trilho.addEventListener('click', () => {
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
+
+    titulo.classList.toggle('dark')
+    if (titulo.classList == 'dark') {
+        titulo.textContent = 'Dark mode'
+    } else {
+        titulo.textContent = 'Light mode'
+    }
 })
